dbbf94c08a20223d59c9dd01dc49e96994ab3df8
Alan Verissimo <alan_verissimo@hotmail.com>
2022-06-04 13:46:53 -0300

    Reactotron
    install reactotron

--- src/index.js
+++ src/index.js
@@ -1,36 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import { View, Text, StyleSheet, StatusBar } from 'react-native';
 
-import api from './services/api';
+import './config/ReactotronConfig';
 
-//nao possuem valor semantico (significado)
-//nao possuem estilização propria 
-//Todos componentes possuem por padrão: "display: flex"
-
-// View: div, footer, header, main, aside, section
-// text: p, span, strong, h1, h2, h3
-
-
-export default function App() {
-    const [projects, setProjects] = useState([]);
-
-    useEffect(() => {
-        api.get('projects').then(response => {
-            console.log(response.data);
-            setProjects(response.data);
-        });
-    }, []);
-
-    return (
-        <>
-        <StatusBar barStyle="light-content" backgroundColor="#7159c1" translucent />
-        
-    <View style={styles.container}>
-        <Text style={styles.title}>Hello GoStack!</Text>
-    </View>
-    </>
-    );
-}
 
 const styles = StyleSheet.create ({
     container: {
@@ -46,3 +18,17 @@ const styles = StyleSheet.create ({
         fontWeight: 'bold'
     },
 });
+
+console.tron.log('Hello World');
+
+export default function App() {
+    return (
+     <>
+     <StatusBar barStyle="light-content" backgroundColor="#7159c1" translucent />
+     
+ <View style={styles.container}>
+     <Text style={styles.title}>Hello GoStack!</Text>
+ </View>
+ </>
+ );
+}
